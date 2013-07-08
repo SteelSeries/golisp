0a28280e3d70be8299678e5cc87072509cb48bde
Dave Astels <dastels@me.com>
2013-07-08 15:54:47 -0500

    Extract repl into the golisp package

---
+++ src/github.com/steelseries/golisp/repl.go
@@ -0,0 +1,42 @@
+// Copyright 2013 SteelSeries ApS. All rights reserved.
+// No license is given for the use of this source code.
+
+// This package impliments a basic LISP interpretor for embedding in a go program for scripting.
+// This file provides a repl
+
+package golisp
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+func Repl() {
+    for true {
+        in := bufio.NewReader(os.Stdin)
+
+        for true {
+            fmt.Printf(">")
+            input, err := in.ReadString('\n')
+            if err != nil {
+                panic(err)
+            }
+            input = strings.TrimRight(input, "\r\n")
+            if input != "" {
+                code, err := Parse(input)
+                if err != nil {
+                    fmt.Printf("Error: %s\n", err)
+                } else {
+                    d, err := Eval(code, Global)
+                    if err != nil {
+                        fmt.Printf("Error in evaluation: %s\n", err)
+                    } else {
+                        fmt.Printf("==> %s\n", String(d))
+                    }
+                }
+            }
+        }
+    }
+}
