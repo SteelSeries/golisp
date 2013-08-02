4e29af29e3c11c4368788a78c811b87c61eb2b0c
Dave Astels <dastels@me.com>
2013-08-02 10:53:45 -0500

    Added flatten that does a one level flattening

--- src/github.com/steelseries/golisp/builtins.go
+++ src/github.com/steelseries/golisp/builtins.go
@@ -89,6 +89,7 @@ func InitBuiltins() {
     MakePrimitiveFunction("length", 1, ListLength)
     MakePrimitiveFunction("cons", 2, ExposedCons)
     MakePrimitiveFunction("reverse", 1, ExposedReverse)
+    MakePrimitiveFunction("flatten", 1, ExposedFlatten)
     MakePrimitiveFunction("append", 2, ExposeAppend)
     MakePrimitiveFunction("append!", 2, ExposeAppendBang)
     MakePrimitiveFunction("copy", 1, ExposeCopy)
@@ -778,6 +779,16 @@ func ExposedReverse(args *Data, env *SymbolTableFrame) (result *Data, err error)
         return
     }
     result = Reverse(val)
+    return
+}
+
+func ExposedFlatten(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+    var val *Data
+    val, err = Eval(Car(args), env)
+    if err != nil {
+        return
+    }
+    result, err = Flatten(val)
     return
 }
 
