4575bd9c8f753c57e9194a1871623a3dad8fb485
Dave Astels <dastels@me.com>
2013-08-02 10:23:14 -0500

    Don't eval map aruments before doign the apply.

--- src/github.com/steelseries/golisp/function.go
+++ src/github.com/steelseries/golisp/function.go
@@ -25,25 +25,29 @@ func (self *Function) String() string {
     return fmt.Sprintf("<func: %s>", self.Name)
 }
 
-func (self *Function) makeLocalBindings(args *Data, argEnv *SymbolTableFrame, localEnv *SymbolTableFrame) (err error) {
+func (self *Function) makeLocalBindings(args *Data, argEnv *SymbolTableFrame, localEnv *SymbolTableFrame, eval bool) (err error) {
     if Length(args) != Length(self.Params) {
         return errors.New("Number of args must equal number of params")
     }
 
     var data *Data
     for p, a := self.Params, args; NotNilP(p); p, a = Cdr(p), Cdr(a) {
-        data, err = Eval(Car(a), argEnv)
-        if err != nil {
-            return
+        if eval {
+            data, err = Eval(Car(a), argEnv)
+            if err != nil {
+                return
+            }
+        } else {
+            data = Car(a)
         }
         localEnv.BindLocallyTo(Car(p), data)
     }
     return nil
 }
 
-func (self *Function) Apply(args *Data, argEnv *SymbolTableFrame) (result *Data, err error) {
+func (self *Function) internalApply(args *Data, argEnv *SymbolTableFrame, eval bool) (result *Data, err error) {
     localEnv := NewSymbolTableFrameBelow(self.Env)
-    err = self.makeLocalBindings(args, argEnv, localEnv)
+    err = self.makeLocalBindings(args, argEnv, localEnv, eval)
     if err != nil {
         return
     }
@@ -54,4 +58,12 @@ func (self *Function) Apply(args *Data, argEnv *SymbolTableFrame) (result *Data,
         }
     }
     return
+}
+
+func (self *Function) Apply(args *Data, argEnv *SymbolTableFrame) (result *Data, err error) {
+    return self.internalApply(args, argEnv, true)
+}
+
+func (self *Function) ApplyWithoutEval(args *Data, argEnv *SymbolTableFrame) (result *Data, err error) {
+    return self.internalApply(args, argEnv, false)
 }
