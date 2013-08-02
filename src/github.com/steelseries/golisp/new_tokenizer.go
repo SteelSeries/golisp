02965fb0ad5f359a395a8e3096039d4503459eab
Dave Astels <dastels@me.com>
2013-08-02 17:26:35 -0500

    Allow * in symbol names

--- src/github.com/steelseries/golisp/new_tokenizer.go
+++ src/github.com/steelseries/golisp/new_tokenizer.go
@@ -45,7 +45,7 @@ func (self *MyTokenizer) NextToken() (token int, lit string) {
 }
 
 func (self *MyTokenizer) isSymbolCharacter(ch rune) bool {
-    return unicode.IsLetter(ch) || unicode.IsNumber(ch) || ch == '-' || ch == '?' || ch == '!' || ch == '_'
+    return unicode.IsLetter(ch) || unicode.IsNumber(ch) || ch == '*' || ch == '-' || ch == '?' || ch == '!' || ch == '_'
 }
 
 func (self *MyTokenizer) readSymbol() (token int, lit string) {
