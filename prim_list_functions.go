19cfd6da0c31223619857b6898b38a5935d74493
Dave Astels <dastels@icloud.com>
2014-06-30 17:21:18 -0500

    Added memq

--- prim_list_functions.go
+++ prim_list_functions.go
@@ -8,77 +8,98 @@
 package golisp
 
 import (
-    "errors"
+	"errors"
 )
 
 func RegisterListFunctionsPrimitives() {
-    MakePrimitiveFunction("map", 2, MapImpl)
-    MakePrimitiveFunction("reduce", 3, ReduceImpl)
+	MakePrimitiveFunction("map", 2, MapImpl)
+	MakePrimitiveFunction("reduce", 3, ReduceImpl)
+	MakePrimitiveFunction("memq", 2, MemqImpl)
 }
 
 func MapImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
-    f, err := Eval(First(args), env)
-    if err != nil {
-        return
-    }
-    if !FunctionP(f) {
-        err = errors.New("Map needs a function as its first argument")
-        return
-    }
+	f, err := Eval(First(args), env)
+	if err != nil {
+		return
+	}
+	if !FunctionP(f) {
+		err = errors.New("Map needs a function as its first argument")
+		return
+	}
 
-    col, err := Eval(Second(args), env)
-    if err != nil {
-        return
-    }
-    if !ListP(col) {
-        err = errors.New("Map needs a list as its second argument")
-        return
-    }
+	col, err := Eval(Second(args), env)
+	if err != nil {
+		return
+	}
+	if !ListP(col) {
+		err = errors.New("Map needs a list as its second argument")
+		return
+	}
 
-    var d []*Data = make([]*Data, 0, Length(col))
-    var v *Data
-    for c := col; NotNilP(c); c = Cdr(c) {
-        v, err = ApplyWithoutEval(f, Cons(Car(c), nil), env)
-        if err != nil {
-            return
-        }
-        d = append(d, v)
-    }
+	var d []*Data = make([]*Data, 0, Length(col))
+	var v *Data
+	for c := col; NotNilP(c); c = Cdr(c) {
+		v, err = ApplyWithoutEval(f, Cons(Car(c), nil), env)
+		if err != nil {
+			return
+		}
+		d = append(d, v)
+	}
 
-    return ArrayToList(d), nil
+	return ArrayToList(d), nil
 }
 
 func ReduceImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
-    f, err := Eval(First(args), env)
-    if err != nil {
-        return
-    }
-    if !FunctionP(f) {
-        err = errors.New("Map needs a function as its first argument")
-        return
-    }
+	f, err := Eval(First(args), env)
+	if err != nil {
+		return
+	}
+	if !FunctionP(f) {
+		err = errors.New("Map needs a function as its first argument")
+		return
+	}
 
-    initial, err := Eval(Second(args), env)
-    if err != nil {
-        return
-    }
-   
-    col, err := Eval(Third(args), env)
-    if err != nil {
-        return
-    }
-    if !ListP(col) {
-        err = errors.New("Map needs a list as its second argument")
-        return
-    }
+	initial, err := Eval(Second(args), env)
+	if err != nil {
+		return
+	}
 
-    result = initial
-    for c := col; NotNilP(c); c = Cdr(c) {
-        result, err = ApplyWithoutEval(f, InternalMakeList(result, Car(c)), env)
-        if err != nil {
-            return
-        }
-    }
+	col, err := Eval(Third(args), env)
+	if err != nil {
+		return
+	}
+	if !ListP(col) {
+		err = errors.New("Map needs a list as its second argument")
+		return
+	}
 
-    return
+	result = initial
+	for c := col; NotNilP(c); c = Cdr(c) {
+		result, err = ApplyWithoutEval(f, InternalMakeList(result, Car(c)), env)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func MemqImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	key, err := Eval(First(args), env)
+	if err != nil {
+		return
+	}
+
+	l, err := Eval(Second(args), env)
+	if err != nil {
+		return
+	}
+
+	for c := l; NotNilP(c); c = Cdr(c) {
+		if IsEqual(key, Car(c)) {
+			return c, nil
+		}
+	}
+
+	return False, nil
 }
