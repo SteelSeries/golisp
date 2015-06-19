e205e18e0225b3196fcdc94af49583842a27cbac
Dave Astels <dastels@Davids-MacBook-Pro.local>
2015-06-19 14:30:02 -0500

    Standard Scheme trimming, substring, prefix, and suffix functions.

--- prim_string.go
+++ prim_string.go
@@ -12,9 +12,17 @@ import (
 	"strings"
 )
 
+const (
+	TrimLeft  = iota
+	TrimBoth  = iota
+	TrimRight = iota
+)
+
 func RegsterStringPrimitives() {
 	MakePrimitiveFunction("split", "2", SplitImpl)
-	MakePrimitiveFunction("trim", "1|2", TrimImpl)
+	MakePrimitiveFunction("string-trim", "1|2", TrimImpl)
+	MakePrimitiveFunction("string-trim-left", "1|2", TrimLeftImpl)
+	MakePrimitiveFunction("string-trim-right", "1|2", TrimRightImpl)
 	MakePrimitiveFunction("string-upcase", "1", StringUpcaseImpl)
 	MakePrimitiveFunction("string-upcase!", "1", StringUpcaseBangImpl)
 	MakePrimitiveFunction("string-downcase", "1", StringDowncaseImpl)
@@ -23,18 +31,22 @@ func RegsterStringPrimitives() {
 	MakePrimitiveFunction("string-capitalize!", "1", StringCapitalizeBangImpl)
 	MakePrimitiveFunction("string-length", "1", StringLengthImpl)
 	MakePrimitiveFunction("string-null?", "1", StringNullImpl)
+	MakePrimitiveFunction("substring", "3", SubstringImpl)
+	MakePrimitiveFunction("substring?", "2", SubstringpImpl)
+	MakePrimitiveFunction("string-prefix?", "2", StringPrefixpImpl)
+	MakePrimitiveFunction("string-suffix?", "2", StringSuffixpImpl)
 }
 
 func SplitImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("trim requires string arguments but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("trim requires a string but was given %s.", String(theString)), env)
 		return
 	}
 
 	theSeparator := Cadr(args)
 	if !StringP(theSeparator) {
-		err = ProcessError(fmt.Sprintf("trim requires string arguments but was given %s.", String(theSeparator)), env)
+		err = ProcessError(fmt.Sprintf("trim requires a string separater but was given %s.", String(theSeparator)), env)
 		return
 	}
 
@@ -46,22 +58,54 @@ func SplitImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	return ArrayToList(ary), nil
 }
 
-func TrimImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+func doTrim(lrb int, args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
 
+	if !StringP(theString) {
+		err = ProcessError(fmt.Sprintf("string-trim requires a string but was given %s.", String(theString)), env)
+		return
+	}
+
+	var trimset string
 	if Length(args) == 2 {
 		theTrimSet := Cadr(args)
-		result = StringWithValue(strings.Trim(StringValue(theString), StringValue(theTrimSet)))
+		if !StringP(theTrimSet) {
+			err = ProcessError(fmt.Sprintf("string-trim requires a string set of trim characters but was given %s.", String(theTrimSet)), env)
+			return
+		}
+
+		trimset = StringValue(theTrimSet)
 	} else {
-		result = StringWithValue(strings.TrimSpace(StringValue(theString)))
+		trimset = " \t\r\n\v\f"
 	}
-	return
+	switch lrb {
+	case TrimLeft:
+		return StringWithValue(strings.TrimLeft(StringValue(theString), trimset)), nil
+	case TrimBoth:
+		return StringWithValue(strings.Trim(StringValue(theString), trimset)), nil
+	case TrimRight:
+		return StringWithValue(strings.TrimRight(StringValue(theString), trimset)), nil
+	default:
+		return theString, nil
+	}
+}
+
+func TrimImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	return doTrim(TrimBoth, args, env)
+}
+
+func TrimLeftImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	return doTrim(TrimLeft, args, env)
+}
+
+func TrimRightImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	return doTrim(TrimRight, args, env)
 }
 
 func StringUpcaseImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("string-upcase requires a string argument but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("string-upcase requires a string but was given %s.", String(theString)), env)
 		return
 	}
 	return StringWithValue(strings.ToUpper(StringValue(theString))), nil
@@ -70,7 +114,7 @@ func StringUpcaseImpl(args *Data, env *SymbolTableFrame) (result *Data, err erro
 func StringUpcaseBangImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("string-upcase requires a string argument but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("string-upcase! requires a string but was given %s.", String(theString)), env)
 		return
 	}
 	return SetStringValue(theString, strings.ToUpper(StringValue(theString))), nil
@@ -79,7 +123,7 @@ func StringUpcaseBangImpl(args *Data, env *SymbolTableFrame) (result *Data, err 
 func StringDowncaseImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("string-downcase requires a string argument but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("string-downcase requires a string but was given %s.", String(theString)), env)
 		return
 	}
 	return StringWithValue(strings.ToLower(StringValue(theString))), nil
@@ -88,7 +132,7 @@ func StringDowncaseImpl(args *Data, env *SymbolTableFrame) (result *Data, err er
 func StringDowncaseBangImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("string-downcase requires a string argument but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("string-downcase! requires a string but was given %s.", String(theString)), env)
 		return
 	}
 	return SetStringValue(theString, strings.ToLower(StringValue(theString))), nil
@@ -105,9 +149,9 @@ func capitalize(s string) string {
 
 func StringCapitalizeImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
-	
+
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("string-capitalize requires a string argument but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("string-capitalize requires a string but was given %s.", String(theString)), env)
 		return
 	}
 	return StringWithValue(capitalize(StringValue(theString))), nil
@@ -115,20 +159,19 @@ func StringCapitalizeImpl(args *Data, env *SymbolTableFrame) (result *Data, err 
 
 func StringCapitalizeBangImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
-	
+
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("string-capitalize requires a string argument but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("string-capitalize! requires a string but was given %s.", String(theString)), env)
 		return
 	}
 	return SetStringValue(theString, capitalize(StringValue(theString))), nil
 }
 
-
 func StringLengthImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
-	
+
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("string-capitalize requires a string argument but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("string-length requires a string but was given %s.", String(theString)), env)
 		return
 	}
 	return IntegerWithValue(int64(len(StringValue(theString)))), nil
@@ -136,10 +179,102 @@ func StringLengthImpl(args *Data, env *SymbolTableFrame) (result *Data, err erro
 
 func StringNullImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	theString := Car(args)
-	
+
 	if !StringP(theString) {
-		err = ProcessError(fmt.Sprintf("string-capitalize requires a string argument but was given %s.", String(theString)), env)
+		err = ProcessError(fmt.Sprintf("string-null? requires a string but was given %s.", String(theString)), env)
 		return
 	}
 	return BooleanWithValue(len(StringValue(theString)) == 0), nil
+}
+
+func SubstringImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	theString := Car(args)
+	if !StringP(theString) {
+		err = ProcessError(fmt.Sprintf("substring requires a string but was given %s.", String(theString)), env)
+		return
+	}
+	stringValue := StringValue(theString)
+
+	startObj := Cadr(args)
+	if !IntegerP(startObj) {
+		err = ProcessError(fmt.Sprintf("substring requires integer start but was given %s.", String(startObj)), env)
+		return
+	}
+	startValue := int(IntegerValue(startObj))
+	if startValue > len(stringValue) {
+		err = ProcessError(fmt.Sprintf("substring requires start < length of the string."), env)
+		return
+	}
+
+	endObj := Caddr(args)
+	if !IntegerP(endObj) {
+		err = ProcessError(fmt.Sprintf("substring requires integer end but was given %s.", String(endObj)), env)
+		return
+	}
+	endValue := int(IntegerValue(endObj))
+	if endValue > len(stringValue) {
+		err = ProcessError(fmt.Sprintf("substring requires end < length of the string."), env)
+		return
+	}
+
+	if startValue > endValue {
+		err = ProcessError(fmt.Sprintf("substring requires start <= end."), env)
+		return
+	}
+
+	return StringWithValue(stringValue[startValue:endValue]), nil
+}
+
+func SubstringpImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	substringObj := Car(args)
+	if !StringP(substringObj) {
+		err = ProcessError(fmt.Sprintf("substring? requires a string but was given %s.", String(substringObj)), env)
+		return
+	}
+	substringValue := StringValue(substringObj)
+
+	theString := Cadr(args)
+	if !StringP(theString) {
+		err = ProcessError(fmt.Sprintf("substring? requires a string but was given %s.", String(theString)), env)
+		return
+	}
+	stringValue := StringValue(theString)
+
+	return BooleanWithValue(strings.Contains(stringValue, substringValue)), nil
+}
+
+func StringPrefixpImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	prefixObj := Car(args)
+	if !StringP(prefixObj) {
+		err = ProcessError(fmt.Sprintf("string-prefix? requires a string but was given %s.", String(prefixObj)), env)
+		return
+	}
+	prefixValue := StringValue(prefixObj)
+
+	theString := Cadr(args)
+	if !StringP(theString) {
+		err = ProcessError(fmt.Sprintf("string-prefix? requires a string but was given %s.", String(theString)), env)
+		return
+	}
+	stringValue := StringValue(theString)
+
+	return BooleanWithValue(strings.HasPrefix(stringValue, prefixValue)), nil
+}
+
+func StringSuffixpImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	suffixObj := Car(args)
+	if !StringP(suffixObj) {
+		err = ProcessError(fmt.Sprintf("string-suffix? requires a string but was given %s.", String(suffixObj)), env)
+		return
+	}
+	suffixValue := StringValue(suffixObj)
+
+	theString := Cadr(args)
+	if !StringP(theString) {
+		err = ProcessError(fmt.Sprintf("string-suffix? requires a string but was given %s.", String(theString)), env)
+		return
+	}
+	stringValue := StringValue(theString)
+
+	return BooleanWithValue(strings.HasSuffix(stringValue, suffixValue)), nil
 }
