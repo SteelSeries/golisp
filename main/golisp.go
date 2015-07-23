cdfdb8099e5fc2fb476d14c78e0258d4bb09a5b1
Dave Astels <dastels@Davids-MacBook-Pro.local>
2015-07-23 13:38:53 -0500

    Finish removing go based testing framework

--- main/golisp.go
+++ main/golisp.go
@@ -10,7 +10,6 @@ import (
 	"flag"
 	"fmt"
 	"github.com/steelseries/golisp"
-	"time"
 )
 
 var (
@@ -18,22 +17,16 @@ var (
 )
 
 func test() {
-	startTime := time.Now()
 	for i := 0; i < flag.NArg(); i = i + 1 {
-		if golisp.VerboseTests {
-			fmt.Printf("Loading %s\n", flag.Arg(i))
-		}
 		_, err := golisp.ProcessFile(flag.Arg(i))
 		if err != nil {
 			//fmt.Printf("Error: %s\n", err)
 		}
 	}
-	golisp.PrintTestResults(time.Since(startTime))
 }
 
 func main() {
 	flag.BoolVar(&runTests, "t", false, "Whether to run tests and exit.  Defaults to false.")
-	flag.BoolVar(&golisp.VerboseTests, "v", false, "Whether tests should be verbose.  Defaults to false.")
 	flag.Parse()
 	if runTests {
 		test()
