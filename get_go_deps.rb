559e8e32181ea34f0466ef38d06cfa1afa496e3c
Dave Astels <dastels@Davids-MacBook-Pro.local>
2015-08-12 12:20:39 -0500

    Remove leftovers from Core

--- get_go_deps.rb
+++ get_go_deps.rb
@@ -3,5 +3,5 @@ if RUBY_PLATFORM =~ /windows/ and ENV[GOARCH] == "386"
   binpath = "bin/windows_386"
 end
 
-system("go get " + ldflags + " github.com/SteelSeries/godep")
+system("go get github.com/SteelSeries/godep")
 system(binpath + "/godep install")
