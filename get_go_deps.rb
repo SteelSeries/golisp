5db2bbef80490e527d40a68bb45a13bc8ba40bca
Dave Astels <dastels@Davids-MacBook-Pro.local>
2015-08-12 12:19:53 -0500

    Remove leftovers from Core

--- get_go_deps.rb
+++ get_go_deps.rb
@@ -1,8 +1,3 @@
-ldflags = ""
-if RUBY_PLATFORM =~ /darwin/
-  ldflags = "-ldflags -linkmode=external"
-end
-
 binpath = "bin"
 if RUBY_PLATFORM =~ /windows/ and ENV[GOARCH] == "386"
   binpath = "bin/windows_386"
@@ -10,4 +5,3 @@ end
 
 system("go get " + ldflags + " github.com/SteelSeries/godep")
 system(binpath + "/godep install")
-system("go install " + ldflags + " bitbucket.org/dastels/goose-sqlite")
