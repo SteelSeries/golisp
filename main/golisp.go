01dce0fd5d1fab09b195ad2c5d169fce62a932d8
Dave Astels <dastels@icloud.com>
2016-10-13 11:44:10 -0400

    Improve startup sequence

--- main/golisp.go
+++ main/golisp.go
@@ -10,12 +10,15 @@ import (
 	"flag"
 	"fmt"
 	. "github.com/dastels/golisp"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 var (
 	runTests     bool = false
 	verboseTests bool = false
+	runRepl bool = false
 )
 
 func test() {
@@ -37,10 +40,46 @@ func test() {
 	ParseAndEval(testCommand)
 }
 
+func loadFile(path string) error {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Printf(" - Error: %s\n", err)
+		return err
+	}
+	if !(filepath.Base(absPath) == ".") && !(filepath.Base(absPath) == "..") {
+		extension := filepath.Ext(path)
+		if extension == ".scm" || extension == ".lsp" {
+			fmt.Printf("  Loading %s", path)
+			_, err = ProcessFile(absPath)
+			if err != nil {
+				fmt.Printf(" - Error: %s\n", err)
+				return err
+			} else {
+				fmt.Printf("\n")
+			}
+		}
+	}
+	return nil
+}
+
+
+func walkFunc(path string, info os.FileInfo, err error) error {
+	return loadFile(path)
+}
+
 func main() {
+	flag.BoolVar(&runRepl, "r", false, "Whether to run the repl after loading golisp code.  Defaults to false.")
 	flag.BoolVar(&runTests, "t", false, "Whether to run tests and exit.  Defaults to false.")
 	flag.BoolVar(&verboseTests, "v", false, "Whether tests should be verbose.  Defaults to false.")
 	flag.Parse()
+
+	fmt.Printf("Loading the lisp directory\n")
+	walkErr := filepath.Walk("lisp", walkFunc)
+	if walkErr != nil {
+		fmt.Printf("Error loading code\n")
+		return
+	}
+
 	if runTests {
 		test()
 	} else {
@@ -50,17 +89,43 @@ func main() {
 				programArgs = flag.Args()[i+1:]
 				break
 			} else {
-				fmt.Printf("Loading %s\n", flag.Arg(i))
-				_, err := ProcessFile(flag.Arg(i))
+				name, err := filepath.Abs(flag.Arg(i))
 				if err != nil {
-					fmt.Printf("Error: %s\n", err)
+					fmt.Printf(" - Error: %s\n", err)
+					return
+				}
+				f, err := os.Open(name)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				defer f.Close()
+				fi, err := f.Stat()
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				switch mode := fi.Mode(); {
+				case mode.IsDir():
+					walkErr = filepath.Walk(flag.Arg(i), walkFunc)
+					if walkErr != nil {
+						fmt.Printf("Error loading code\n")
+						return
+					}
+				case mode.IsRegular():
+					loadErr := loadFile(flag.Arg(i))
+					if loadErr != nil {
+						return
+					}
 				}
 			}
 		}
-
+		
 		mainValue := Global.ValueOf(Intern("main"))
 
-		if FunctionP(mainValue) {
+		if runRepl || !FunctionP(mainValue) {
+			Repl()
+		} else {
 			println("Calling main")
 			args := make([]*Data, 0, len(flag.Args()))
 			for _, arg := range programArgs {
@@ -73,8 +138,6 @@ func main() {
 			} else {
 				fmt.Printf("==> %s\n", String(result))
 			}
-		} else {
-			Repl()
 		}
 	}
 }
