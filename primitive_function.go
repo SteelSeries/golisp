c258fd9b744ca2399de7302f08e783798fd06b32
Dave Astels <dastels@icloud.com>
2016-12-05 22:07:49 -0500

    Add argument typing support

--- primitive_function.go
+++ primitive_function.go
@@ -17,13 +17,18 @@ type PrimitiveFunction struct {
 	Name         string
 	Special      bool
 	NumberOfArgs string
+	ArgTypes     []uint32
 	Body         func(d *Data, env *SymbolTableFrame) (*Data, error)
 }
 
-func MakePrimitiveFunction(name string, argCount string, function func(*Data, *SymbolTableFrame) (*Data, error)) {
-	f := &PrimitiveFunction{Name: name, Special: false, NumberOfArgs: argCount, Body: function}
+func MakeTypedPrimitiveFunction(name string, argCount string, function func(*Data, *SymbolTableFrame) (*Data, error), types []uint32) {
+	f := &PrimitiveFunction{Name: name, Special: false, NumberOfArgs: argCount, ArgTypes: types, Body: function}
 	sym := Global.Intern(name)
 	Global.BindTo(sym, PrimitiveWithNameAndFunc(name, f))
+}
+
+func MakePrimitiveFunction(name string, argCount string, function func(*Data, *SymbolTableFrame) (*Data, error)) {
+	MakeTypedPrimitiveFunction(name, argCount, function, make([]uint32, 0, 0))
 }
 
 func MakeSpecialForm(name string, argCount string, function func(*Data, *SymbolTableFrame) (*Data, error)) {
@@ -61,6 +66,26 @@ func (self *PrimitiveFunction) checkArgumentCount(argCount int) bool {
 	return false
 }
 
+func (self *PrimitiveFunction) checkArgumentTypes(args []*Data) int {
+	for i := 0; i < len(self.ArgTypes) && i < len(args); i = i + 1 {
+		arg := args[i]
+		if (TypeOf(arg) & self.ArgTypes[i]) == 0 {
+			return i
+		}
+	}
+	return -1
+}
+
+func (self *PrimitiveFunction) typesToString(types uint32) string {
+	typeNames := make([]string, 0, 8)
+	for mask := uint32(0x00000001); mask != 0x80000000; mask <<= 1 {
+		if (types & mask) != 0 {
+			typeNames = append(typeNames, TypeName(mask))
+		}
+	}
+	return strings.Join(typeNames, " or ")
+}
+
 func (self *PrimitiveFunction) Apply(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	if !self.checkArgumentCount(Length(args)) {
 		err = fmt.Errorf("Wrong number of args to %s, expected %s but got %d.", self.Name, self.NumberOfArgs, Length(args))
@@ -80,6 +105,12 @@ func (self *PrimitiveFunction) Apply(args *Data, env *SymbolTableFrame) (result 
 		}
 
 		argArray = append(argArray, argValue)
+	}
+
+	argCheckResult := self.checkArgumentTypes(argArray)
+	if argCheckResult != -1 {
+		err = fmt.Errorf("Wrong argument type for argument %d; expected %s but got the %s: %s", argCheckResult, self.typesToString(self.ArgTypes[argCheckResult]), TypeName(TypeOf(argArray[argCheckResult])), String(argArray[argCheckResult]))
+		return
 	}
 
 	localGuid := atomic.AddInt64(&ProfileGUID, 1) - 1
