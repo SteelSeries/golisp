8a9559440729b8b625be072ac2c6e24b29fe11a7
Dave Astels <dastels@icloud.com>
2016-12-13 22:44:51 -0500

    Fix the lisp test runner and add the GOLISPHOME env var

--- lisp_test.go
+++ lisp_test.go
@@ -8,7 +8,10 @@
 package golisp
 
 import (
+	"fmt"
 	. "gopkg.in/check.v1"
+	"os"
+	"path/filepath"
 )
 
 type LispSuite struct {
@@ -16,11 +19,42 @@ type LispSuite struct {
 
 var _ = Suite(&LispSuite{})
 
+func loadFile(path string) error {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Printf(" - Error: %s\n", err)
+		return err
+	}
+	if !(filepath.Base(absPath) == ".") && !(filepath.Base(absPath) == "..") {
+		extension := filepath.Ext(path)
+		if extension == ".scm" || extension == ".lsp" {
+			fmt.Printf("  Loading %s", path)
+			_, err = ProcessFile(absPath)
+			if err != nil {
+				fmt.Printf(" - Error: %s\n", err)
+				return err
+			} else {
+				fmt.Printf("\n")
+			}
+		}
+	}
+	return nil
+}
+
+func walkFunc(path string, info os.FileInfo, err error) error {
+	return loadFile(path)
+}
+
 func (s *LispSuite) TestLisp(c *C) {
 	testCommand := "(run-all-tests \"tests\" #t)"
-	_, err := ProcessFile("lisp/conditional.scm")
+	err := filepath.Walk(os.ExpandEnv("$GOLISPHOME/lisp"), walkFunc)
+	if err != nil {
+		fmt.Printf("Error loading code\n")
+		return
+	}
+
 	c.Assert(err, IsNil)
-	_, err = ProcessFile("lisp/testing.scm")
+	_, err = ProcessFile(os.ExpandEnv("$GOLISPHOME/tools/testing.scm"))
 	c.Assert(err, IsNil)
 	data, err := ParseAndEval(testCommand)
 	c.Assert(err, IsNil)
