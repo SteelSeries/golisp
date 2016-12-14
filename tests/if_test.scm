c96ed6965d579eb29b481c93c1aabac023627d58
Dave Astels <dastels@icloud.com>
2016-12-13 22:45:26 -0500

    it requires strings now

--- tests/if_test.scm
+++ tests/if_test.scm
@@ -6,45 +6,45 @@
 
          ()
          
-         (it when
+         (it "when"
              (assert-eq (when #t 1 2 3 4) 4)
              (assert-eq (when #f 1 2 3 4) nil))
 
-         (it when-arbitrary-conditional
+         (it "when-arbitrary-conditional"
              (assert-eq (when (< 2 4) 1 2 3 4) 4)
              (assert-eq (when (> 2 4) 1 2 3 4) nil))
 
 
-         (it when-dont-eval
+         (it "when-dont-eval"
              (set! when1 1)
              (assert-eq (when #f (set! when1 42) 1) nil)
              (assert-eq when1 1))
 
-         (it when-eval
+         (it "when-eval"
              (set! when1 1)
              (assert-eq (when #t (set! when1 42) 1) 1)
              (assert-eq when1 42))
 
 
 
-         (it unless
+         (it "unless"
              (assert-eq (unless #f 1 2 3 4)
                         4)
              (assert-nil (unless #t 1 2 3 4)))
 
-         (it unless-arbitrary-conditional
+         (it "unless-arbitrary-conditional"
              (assert-eq (unless (> 2 4) 1 2 3 4)
                         4)
              (assert-nil (unless (< 2 4) 1 2 3 4)))
 
 
-         (it unless-dont-eval
+         (it "unless-dont-eval"
              (set! when1 1)
              (assert-nil (unless #t (set! when1 42) 1))
              (assert-eq when1
                         1))
 
-         (it unless-eval
+         (it "unless-eval"
              (set! when1 1)
              (assert-eq (unless #f (set! when1 42) 1)
                         1)
