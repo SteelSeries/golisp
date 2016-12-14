28c2d1350a6100ed4852b0b89732aec90204df2f
Dave Astels <dastels@icloud.com>
2016-12-13 22:45:58 -0500

    Not needed since test preloads lisp directory

--- tools/testing.scm
+++ tools/testing.scm
@@ -4,8 +4,6 @@
 ;;; Use of this source code is governed by a BSD-style
 ;;; license that can be found in the LICENSE file.
 
-(load "lisp/lists.scm")
-
 (define number-of-passes 0)
 (define number-of-failures 0)
 (define number-of-errors 0)
