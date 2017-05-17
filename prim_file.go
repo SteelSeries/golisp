245474bb602b2a6141bfad828d842e5fafda5e67
Dave Astels <dastels@daveastels.com>
2017-05-16 22:17:09 -0400

    Add more file functions

--- prim_file.go
+++ prim_file.go
@@ -9,17 +9,30 @@ package golisp
 
 import (
 	"os"
+	"path/filepath"
 )
 
 func RegisterFilePrimitives() {
 	MakeTypedPrimitiveFunction("file-exists?", "1", fileExistsImpl, []uint32{StringType})
+	MakeTypedPrimitiveFunction("file-directory?", "1", directoryReadImpl, []uint32{StringType})
 	MakeTypedPrimitiveFunction("file-modification-time", "1", fileModificationTimeImpl, []uint32{StringType})
+	MakeTypedPrimitiveFunction("directory-read", "1", listDirectoryImpl, []uint32{StringType})
 }
 
 func fileExistsImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
 	_, ferr := os.Stat(StringValue(Car(args)))
-	result = BooleanWithValue(!os.IsNotExist(ferr))
-	return
+	return BooleanWithValue(!os.IsNotExist(ferr)), nil
+}
+
+func directoryReadImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	info, err := os.Stat(StringValue(Car(args)))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return LispFalse, nil
+		}
+		return
+	}
+	return BooleanWithValue(info.IsDir()), nil
 }
 
 func fileModificationTimeImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
@@ -31,4 +44,21 @@ func fileModificationTimeImpl(args *Data, env *SymbolTableFrame) (result *Data, 
 		return
 	}
 	return IntegerWithValue(info.ModTime().Unix()), nil
+}
+
+func listDirectoryImpl(args *Data, env *SymbolTableFrame) (result *Data, err error) {
+	dir := StringValue(First(args))
+	pattern := filepath.Join(dir, "*")
+
+	filenames, err := filepath.Glob(pattern)
+	if err != nil {
+		return
+	}
+
+	names := make([]*Data, len(filenames))
+	for index, fname := range filenames {
+		fname, _ = filepath.Abs(fname)
+		names[index] = StringWithValue(fname)
+	}
+	return ArrayToList(names), nil
 }
