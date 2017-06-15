2f75f096b6471ec3887930985687219c9f06150e
Dave Astels <dastels@daveastels.com>
2017-06-15 19:32:07 -0400

    Have assembler & optimizer called in the compiler environment

--- tests/assembler_test.scm
+++ tests/assembler_test.scm
@@ -2,33 +2,36 @@
 
 (load (string-join (list (get-env "GOLISPHOME") "tools" "compiler.scm") "/"))
 
+(define (a1 c) (eval `(asm-first-pass ',c) ***COMPILER-ENVIRONMENT***))
+(define (a2 c n l) (eval `(asm-second-pass ',c ',n ',l) ***COMPILER-ENVIRONMENT***))
+(define (ctbc c) (eval `(convert-to-bytecode ',c) ***COMPILER-ENVIRONMENT***))
 
 (context "Assembler first pass"
 
 		 ()
 
 		 (it "can measure the length of empty code"
-			 (assert-eq (first (asm-first-pass '()))
+			 (assert-eq (first (a1 '()))
 						0))
 
 		 (it "can measure the length of short code"
-			 (assert-eq (first (asm-first-pass '((a))))
+			 (assert-eq (first (a1 '((a))))
 						1))
 
 		 (it "can measure the length of longer code"
-			 (assert-eq (first (asm-first-pass '((a) (b) (c) (d))))
+			 (assert-eq (first (a1 '((a) (b) (c) (d))))
 						4))
 
 		 (it "finds no labels when there aren't any"
-			 (assert-eq (second (asm-first-pass '()))
+			 (assert-eq (second (a1 '()))
 						'()))
 		 
-		 (it "finds 1 labels when there is 1"
-			 (assert-eq (second (asm-first-pass '((a) l1 (b))))
+		 (it "finds 1 label when there is 1"
+			 (assert-eq (second (a1 '((a) l1 (b))))
 						'((l1 . 1))))
 		 
 		 (it "finds 3 labels when there are 3"
-			 (assert-eq (second (asm-first-pass '((a) L1 (b) L2 (c) L3 (d))))
+			 (assert-eq (second (a1 '((a) L1 (b) L2 (c) L3 (d))))
 						'((L3 . 3) (L2 . 2) (L1 . 1))))
 		 
 		 )
@@ -38,14 +41,14 @@
 		 ()
 
 		 (it "returns a vector"
-			 (assert-eq (type-of (asm-second-pass '() 0 '())) 'vector))
+			 (assert-eq (type-of (a2 '() 0 '())) 'vector))
 
 		 (it "doesn't change things that don't use labels"
-			 (assert-eq (asm-second-pass '((NIL) (RETURN)) 2 '())
+			 (assert-eq (a2 '((NIL) (RETURN)) 2 '())
 						#((NIL) (RETURN))))
 
 		 (it "replaces label references"
-			 (assert-eq (asm-second-pass '((NIL) (JUMP L2) (FJUMP L3) (RETURN)) 4 '((L3 . 1) (L2 . 4)))
+			 (assert-eq (a2 '((NIL) (JUMP L2) (FJUMP L3) (RETURN)) 4 '((L3 . 1) (L2 . 4)))
 						#((NIL) (JUMP 4) (FJUMP 1) (RETURN))))
 		 )
 
@@ -54,9 +57,9 @@
 		 ()
 
 		 (it "converts instructions to vectors"
-			 (assert-eq (type-of (vector-first (convert-to-bytecode #((ONE) (TWO) (PLUS) (RETURN)))))
+			 (assert-eq (type-of (vector-first (ctbc #((ONE) (TWO) (PLUS) (RETURN)))))
 						'vector))
 		 
 		 (it "converts simple instructions"
-			 (assert-eq (convert-to-bytecode #((ONE) (TWO) (PLUS) (RETURN)))
+			 (assert-eq (ctbc #((ONE) (TWO) (PLUS) (RETURN)))
 						#(#(1) #(2) #(32) #(16)))))
--- tests/optimizer_test.scm
+++ tests/optimizer_test.scm
@@ -2,97 +2,98 @@
 
 (load (string-join (list (get-env "GOLISPHOME") "tools" "compiler.scm") "/"))
 
+(define (opt c) (eval `(optimize ',c) ***COMPILER-ENVIRONMENT***))
 
 (context "Optimizer"
 
 		 ()
 
 		 (it "removes unreferenced labels"
-			 (assert-eq (optimize '((POP) L1 (RETURN))) '((POP) (RETURN))))
+			 (assert-eq (opt '((POP) L1 (RETURN))) '((POP) (RETURN))))
 
 
 		 (it "simplifies gset-pop-gvar"
-			 (assert-eq (optimize '((GSET a) (POP) (GVAR a)))
+			 (assert-eq (opt '((GSET a) (POP) (GVAR a)))
 						'((GSET a))))
 
 		 (it "simplifies lset-pop-lget"
-			 (assert-eq (optimize '((LSET a) (POP) (LVAR a)))
+			 (assert-eq (opt '((LSET a) (POP) (LVAR a)))
 						'((LSET a))))
 
 		 (it "removes dead code between a JUMP and the next label"
-			 (assert-eq (optimize '((JUMP L2) (JUMP L1) (ONE) L2 (RETURN)))
+			 (assert-eq (opt '((JUMP L2) (JUMP L1) (ONE) L2 (RETURN)))
 						'((JUMP L2) (JUMP L1) L2 (RETURN))))
 
 		 (it "removes a trivial jump"
-			 (assert-eq (optimize '((ONE) (JUMP L1) L1 (RETURN)))
+			 (assert-eq (opt '((ONE) (JUMP L1) L1 (RETURN)))
 						'((ONE) (RETURN))))
 
 		 (it "retargets jumps to avoid hopping"
-			 (assert-eq (optimize '((JUMP L1) (POP) L1 (JUMP L2)))
+			 (assert-eq (opt '((JUMP L1) (POP) L1 (JUMP L2)))
 						'((JUMP L2) (JUMP L2))))
 		 
 		 (it "retargets on-true jumps to avoid hopping"
-			 (assert-eq (optimize '((TJUMP L1) (POP) L1 (JUMP L2)))
+			 (assert-eq (opt '((TJUMP L1) (POP) L1 (JUMP L2)))
 						'((TJUMP L2) (POP) (JUMP L2))))
 
 		 (it "retargets on-false jumps to avoid hopping"
-			 (assert-eq (optimize '((FJUMP L1) (POP) L1 (JUMP L2)))
+			 (assert-eq (opt '((FJUMP L1) (POP) L1 (JUMP L2)))
 						'((FJUMP L2) (POP) (JUMP L2))))
 
 		 (it "collapses (TRUE) (NOT) to (FALSE)"
-			 (assert-eq (optimize '((TRUE) (NOT)))
+			 (assert-eq (opt '((TRUE) (NOT)))
 						'((FALSE))))
 
 		 (it "Removes TJUMPs that are never taken"
-			 (assert-eq (optimize '((FALSE) (TJUMP L) (POP)))
+			 (assert-eq (opt '((FALSE) (TJUMP L) (POP)))
 						'((POP))))
 
 		 (it "Makes an always taken TJUMP into a JUMP"
-			 (assert-eq (optimize '((TRUE) (TJUMP L)))
+			 (assert-eq (opt '((TRUE) (TJUMP L)))
 						'((JUMP L))))
 
 		 (it "collapses (ZERO) (NOT) to (FALSE)"
-			 (assert-eq (optimize '((ZERO) (NOT)))
+			 (assert-eq (opt '((ZERO) (NOT)))
 						'((FALSE))))
 
 		 (it "Makes an always taken TJUMP into a JUMP"
-			 (assert-eq (optimize '((ZERO) (TJUMP L)))
+			 (assert-eq (opt '((ZERO) (TJUMP L)))
 						'((JUMP L))))
 
 		 (it "collapses (ONE) (NOT) to (FALSE)"
-			 (assert-eq (optimize '((ONE) (NOT)))
+			 (assert-eq (opt '((ONE) (NOT)))
 						'((FALSE))))
 
 		 (it "Makes an always taken TJUMP into a JUMP"
-			 (assert-eq (optimize '((ONE) (TJUMP L)))
+			 (assert-eq (opt '((ONE) (TJUMP L)))
 						'((JUMP L))))
 
 		 (it "collapses (TWO) (NOT) to (FALSE)"
-			 (assert-eq (optimize '((TWO) (NOT)))
+			 (assert-eq (opt '((TWO) (NOT)))
 						'((FALSE))))
 
 		 (it "Makes an always taken TJUMP into a JUMP"
-			 (assert-eq (optimize '((TWO) (TJUMP L)))
+			 (assert-eq (opt '((TWO) (TJUMP L)))
 						'((JUMP L))))
 
 		 (it "collapses (NIL) (NOT) to (FALSE)"
-			 (assert-eq (optimize '((NIL) (NOT)))
+			 (assert-eq (opt '((NIL) (NOT)))
 						'((FALSE))))
 
 		 (it "Makes an always taken TJUMP into a JUMP"
-			 (assert-eq (optimize '((NIL) (TJUMP L)))
+			 (assert-eq (opt '((NIL) (TJUMP L)))
 						'((JUMP L))))
 
 		 (it "collapses (FALSE) (NOT) to (TRUE)"
-			 (assert-eq (optimize '((FALSE) (NOT)))
+			 (assert-eq (opt '((FALSE) (NOT)))
 						'((TRUE))))
 
 		 (it "Removes FJUMPs that are never taken"
-			 (assert-eq (optimize '((TRUE) (FJUMP L) (POP)))
+			 (assert-eq (opt '((TRUE) (FJUMP L) (POP)))
 						'((POP))))
 
 		 (it "Makes an always taken FUMP into a JUMP"
-			 (assert-eq (optimize '((FALSE) (FJUMP L)))
+			 (assert-eq (opt '((FALSE) (FJUMP L)))
 						'((JUMP L))))
 
 		 )
